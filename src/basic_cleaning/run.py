361654aac2056ccd01a2ee8c5fc4fe6d23da9b70
Xue Li <48598877+lixuevicky@users.noreply.github.com>
2025-06-01 23:50:35 -0700

    Added_longitude_latitude_boundary

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,8 @@ def go(args):
     logger.info("Removing Outliers")
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
